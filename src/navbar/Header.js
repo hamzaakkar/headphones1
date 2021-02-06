ee518cefa15c8230141413c3b5d1f38172d58e1c
hamzaakkar <hamzaakkar2@gmail.com>
2021-02-06 23:39:08 +0200

    newupdatenavbar

--- src/navbar/Header.js
+++ src/navbar/Header.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import { Navbar,Nav,Form,FormControl,Button, } from 'react-bootstrap';
+import { Navbar,Nav,Form} from 'react-bootstrap';
 import styles from "./Header.module.css"
 import Logo from "./logo (1).svg"
+import Vector from "./Vector.svg"
+import Group from "./Group.svg"
+import karoussa from "./karoussa.svg"
+import union from "./Union.svg"
+
 function Header() {
   return (
     <div>
+      <Navbar.Brand href="#union" className={styles.union}><img src={union} alt=""/></Navbar.Brand>
+      
       <Navbar className={styles.nav} bg="" variant="dark">
     <Navbar.Brand href="#home"><img src={Logo} alt=""/></Navbar.Brand>
+    
     <Nav className={styles.menu}variant="dark">
 
-      <Nav.Link href="#headphones" style={{ color: 'black' }}>Headphones</Nav.Link>
-      <Nav.Link href="#speakers"style={{ color: 'black' }}>Speakers</Nav.Link>
-      <Nav.Link href="#audio"style={{ color: 'black' }}>Audio Sunglasses</Nav.Link>
-      <Nav.Link href="#support"style={{ color: 'black' }}>Support</Nav.Link>
+      <Nav.Link href="#headphones" className={styles.headphones}>Headphones</Nav.Link>
+      <Nav.Link href="#speakers" className={styles.speakers}>Speakers</Nav.Link>
+      <Nav.Link href="#audio"className={styles.audio}>Audio Sunglasses</Nav.Link>
+      <Nav.Link href="#support"className={styles.support}>Support</Nav.Link>
+
       
     </Nav>
+
+    <Navbar.Brand href="#search" className={styles.Vector}><img src={Vector} alt=""/></Navbar.Brand>
+    <Navbar.Brand href="#Group" className={styles.Group}><img src={Group} alt=""/></Navbar.Brand>
+    <Navbar.Brand href="#karoussa" className={styles.karoussa}><img src={karoussa} alt=""/></Navbar.Brand>
+    
+
+
+
+    
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
+
+   
     </Form>
   </Navbar>
+  
+
 
     </div>
   )
